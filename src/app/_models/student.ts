3ad573de96b1f8d5dd3ca7ae28dc602d10b41178
Ayman Salah <ayman.1551.salah@gmail.com>
2023-03-08 20:56:28 +0200

    Created the model

---
+++ src/app/_models/student.ts
@@ -0,0 +1,10 @@
+export class Student {
+  // This is the Model Class
+  constructor(public id:number , public name:string , public age:number)
+  // constructor(public id:number , public name?:string , public age:number=99)
+  // If we Use Question Mark , it means this Field is optional like name
+  // We Can also Make Default Values if we Want So Like age
+  {
+    // NOTHING more is Needed , We Can Now just Use the CTOR to Create Objects
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,8 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+// TODO : Adding the Bootstrap to the angular.json File  ;
+// TODO: Adding the Jquery if Needed
+
