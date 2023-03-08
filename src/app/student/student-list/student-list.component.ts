198aa1a1a5ad03a92508b46e295425e84ca92bde
Ayman Salah <ayman.1551.salah@gmail.com>
2023-03-08 21:17:52 +0200

    List Component

--- src/app/student/student-list/student-list.component.ts
+++ src/app/student/student-list/student-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Student } from 'src/app/_models/student';
 
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
   styleUrls: ['./student-list.component.css']
 })
+
+//
 export class StudentListComponent {
+
+  students : Student[] = [
+    // A List Of Students
+    new Student(1,"Ayman" , 28) ,
+    new Student(2,"Khaled" , 26),
+    new Student(3,"Suzan" , 30)
+  ] ;
 
 }
